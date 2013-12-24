72646dce5824837c680da16f3e8d032951de8a28
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-12-23 21:06:27 -0800

    setup views

--- config.ru
+++ config.ru
@@ -1,3 +1,9 @@
 $stdout.sync = true
 
+set :app_file, __FILE__
+
+configure do
+  set :views, File.join(Sinatra::Application.root, "lib", "views")
+end
+
 run Sinatra::Application
--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require 'active_record'
 require 'pg'
 require 'pathname'
 require 'uri'
+require 'erb'
 
 APP_ROOT = Pathname.new(File.expand_path('../', __FILE__))
 APP_NAME = APP_ROOT.basename.to_s
