69254e1f96fec88b1281b44a51b3abc7e2cee485
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-12-23 21:18:57 -0800

    tweak app_name

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require 'pathname'
 require 'uri'
 require 'erb'
 
-APP_ROOT = Pathname.new(File.expand_path('../', __FILE__))
+APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 APP_NAME = APP_ROOT.basename.to_s
 
 require File.expand_path(File.join(File.dirname(__FILE__), 'db.rb'))
