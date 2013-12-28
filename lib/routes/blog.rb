5a922a118e791565a2ffecc83c66c6e36660b0f1
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-12-27 19:00:20 -0800

    flesh out blog routes

--- lib/routes/blog.rb
+++ lib/routes/blog.rb
@@ -1,34 +1,41 @@
 # Index
 get '/posts' do
-
+  @posts = Post.all
+  erb :"blog/index"
 end
 
-# Read
+# Show
 get '/posts/:id' do
-
+  @post = Post.find(params[:id])
+  erb :"blog/show"
 end
 
 # New
 get '/posts/new' do
-
+  erb :"blog/new"
 end
 
 # Edit
 get '/posts/:id/edit' do
-
+  @post = Post.find(params[:id])
+  erb :"blog/edit"
 end
 
 # Create
 post '/posts' do
-
+  Post.create(params[:id])
+  redirect '/posts'
 end
 
 # Update
 put '/posts/:id' do
-
+  post = Post.find(params[:id])
+  post.update_attributes(params[:post])
+  redirect "/posts/#{params[:id]}"
 end
 
 # Delete
 delete '/posts/:id' do
-
+  Post.destroy(params[:id])
+  redirect '/posts'
 end
